572f2d519dc24cd79d128ed9ab4415ac0cafd764
OfirK <magicofir11@gmail.com>
2023-03-25 18:49:25 +0300

    Move product gallery into separate component

--- src/Orders.js
+++ src/Orders.js
@@ -1,58 +1,17 @@
-import { ExpandLess as ExpandLessIcon, ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
-import { Card, CardContent, Grid, IconButton, Stack, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import { Card, CardContent, Stack, Typography } from '@mui/material';
+import React from 'react';
 import { useQuery } from "react-query";
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { getOrders } from "./api_calls";
-import ProductDialog from './ProductDialog';
+import ExpandableProductGallery from './ExpandableProductGallery';
 
 const Orders = () => {
-  const [selectedProduct, setSelectedProduct] = useState(null);
-  const [expandedOrders, setExpandedOrders] = useState([]);
-
   const { data: orders, isFetching: isLoadingOrders } = useQuery({
     queryKey: 'orders',
     queryFn: getOrders,
     placeholderData: [],
   });
-
-  const handleProductClick = (product) => {
-    setSelectedProduct(product);
-  };
-
-  const handleCloseDialog = () => {
-    setSelectedProduct(null);
-  };
-
-  const handleExpandOrder = (order) => {
-    console.log(expandedOrders);
-    if (expandedOrders.includes(order)) {
-      setExpandedOrders((prevExpandedOrders) => prevExpandedOrders.filter((o) => o !== order));
-    } else {
-      setExpandedOrders((prevExpandedOrders) => [...prevExpandedOrders, order]);
-    }
-  };
-
-  const MAX_PRODUCTS = 8;
-
-  const getProductsToShow = (products) => {
-    if (products.length <= MAX_PRODUCTS) {
-      return products;
-    } else {
-      return products.slice(0, MAX_PRODUCTS);
-    }
-  };
-
-  const getMoreProducts = (products) => {
-    if (products.length <= MAX_PRODUCTS) {
-      return [];
-    } else {
-      return products.slice(MAX_PRODUCTS);
-    }
-  };
-
-  const shouldShowAllImages = (order) => expandedOrders.includes(order);
 
   return (
     <Stack spacing={2} sx={{ flexGrow: 1, p: 2, m: "0 auto", maxWidth: 1200 }} dir="rtl">
@@ -67,43 +26,12 @@ const Orders = () => {
             <Typography variant="subtitle1" gutterBottom>
               {order.description}
             </Typography>
-            <Grid container spacing={2}>
-              {(shouldShowAllImages(order) ? order.ordered_products : getProductsToShow(order.ordered_products)).map((product) => (
-                <Grid item key={product.did}>
-                  <ProductImage src={product.image_url_list ? product.image_url_list[0] : null} alt={product.name} onClick={() => handleProductClick(product)} />
-                </Grid>
-              ))}
-              {order.ordered_products.length > MAX_PRODUCTS && (
-                <Grid item>
-                  <IconButton onClick={() => handleExpandOrder(order)}>
-                    {shouldShowAllImages(order) ? (
-                      <React.Fragment>
-                        <Typography>Show less</Typography>
-                        <ExpandLessIcon />
-                      </React.Fragment>
-                    ) : (
-                      <React.Fragment>
-                        <Typography>Show more</Typography>
-                        <ExpandMoreIcon />
-                      </React.Fragment>
-                    )}
-                  </IconButton>
-                </Grid>
-              )}
-            </Grid>
-            {shouldShowAllImages(order) && (
-              <Grid container spacing={2}>
-                {getMoreProducts(order.ordered_products).map((product) => (
-                  <Grid item key={product.id}>
-                    <ProductImage src={product.image_url_list ? product.image_url_list[0] : null} alt={product.name} onClick={() => handleProductClick(product)} />
-                  </Grid>
-                ))}
-              </Grid>
-            )}
+            <ExpandableProductGallery
+              products={order.ordered_products}
+            />
           </CardContent>
         </Card>
       ))}
-      <ProductDialog open={selectedProduct !== null} onClose={handleCloseDialog} product={selectedProduct} />
     </Stack>
   );
 };
