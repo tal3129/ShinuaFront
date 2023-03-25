24bfaf52fe52f516d7fb176f5c41367cee288eee
OfirK <magicofir11@gmail.com>
2023-03-25 19:06:53 +0300

    Add tabs component

--- src/Orders.js
+++ src/Orders.js
@@ -1,4 +1,4 @@
-import { Card, CardContent, Stack, Typography } from '@mui/material';
+import { Card, CardContent, Stack, Tab, Tabs, Typography } from '@mui/material';
 import React from 'react';
 import { useQuery } from "react-query";
 import { Link } from 'react-router-dom';
@@ -15,14 +15,18 @@ const Orders = () => {
 
   return (
     <Stack spacing={2} sx={{ flexGrow: 1, p: 2, m: "0 auto", maxWidth: 1200 }} dir="rtl">
+      <Tabs aria-label="order tabs" centered value={0}>
+        <Tab label="הזמנות פתוחות" />
+        <Tab label="הזמנות שהסתיימו"/>
+      </Tabs>
       {orders.map((order) => (
         <Card key={order.name}>
           <CardContent>
-            <Link key={order.did} to={{ pathname: `/orders/${order.did}` }} state={{ order }}>
+            <StyledLink key={order.did} to={{ pathname: `/orders/${order.did}` }} state={{ order }}>
               <Typography variant="h5" gutterBottom>
                 {order.name}
               </Typography>
-            </Link>
+            </StyledLink>
             <Typography variant="subtitle1" gutterBottom>
               {order.description}
             </Typography>
@@ -39,6 +43,11 @@ const Orders = () => {
 const ProductImage = styled.img`
   cursor: pointer;
   height: 100px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
 `;
 
 export default Orders;
